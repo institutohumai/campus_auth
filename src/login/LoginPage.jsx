a0be05004bdc24f7f3c56ff4a8af368f4869628d
Pablo G <pablosgomez50@gmail.com>
2023-08-15 19:32:32 -0300

    Borrar fondo en MediumLayout. Agregar h3 en LoginPage.

--- src/login/LoginPage.jsx
+++ src/login/LoginPage.jsx
@@ -264,6 +264,7 @@ class LoginPage extends React.Component {
           {activationMsgType && <AccountActivationMessage messageType={activationMsgType} />}
           {this.props.resetPassword && !this.props.loginError ? <ResetPasswordSuccess /> : null}
           <Form name="sign-in-form" id="sign-in-form">
+            <h3>Test Login personalizado LoginPage.jsx</h3>
             <FormGroup
               name="emailOrUsername"
               value={this.state.emailOrUsername}
